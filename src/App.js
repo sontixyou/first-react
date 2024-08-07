9324b745dae3fc9d934da4704cf09add81890da7
kenken <kengoviolinphoto@gmail.com>
2024-08-07 23:06:20 +0900

    Add markup tags

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <article>
+        <h1>sontixyou first component</h1>
+        <ol>
+          <li>Rust</li>
+          <li>axum</li>
+          <li>sea-orm</li>
+        </ol>
+      </article>
     </div>
   );
 }
